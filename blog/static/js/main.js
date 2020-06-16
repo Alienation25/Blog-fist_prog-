a89817a148074572fad7427655989fb5d22388be
Alienation25 <yarisvet2000@gmail.com>
2020-06-16 05:21:06 +0300

    create bootsrap and js to the form load in the all dire

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -1,2 +1,14 @@
-btnGetKeyWord = document.getElementsByClassName("navbar navbar-dark bg-dark shadow-sm");
-console.log(btnGetKeyWord)
+let button_head = document.querySelectorAll("div.navbar.navbar-dark.bg-dark.shadow-sm div.container.d-flex.justify-content-between button.navbar-toggler");
+let head_block=document.querySelectorAll("#navbarHeader");
+
+//bg-dark collapse show
+//bg-dark collapsing show
+console.log(head_block[0])
+button_head[0].onclick=function(){
+    if(head_block[0].className == "bg-dark collapse"){
+    head_block[0].className="bg-dark collapse show";
+    }else{
+    head_block[0].className="bg-dark collapse"
+    }
+    
+}  
