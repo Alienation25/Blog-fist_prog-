273c5d89c8434ae9175527bfa17def30f34d13df
Alienation25 <yarisvet2000@gmail.com>
2020-06-17 04:35:21 +0300

    create img add in base

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,10 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    button_url=models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(null=True, blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +18,6 @@ class Post(models.Model):
 
     def __str__(self):#Перегрузка функция для  преобразование объекта к строковому представлению, вызывается, когда объект передается функциям print() и str()
         return self.title
+
+
+
